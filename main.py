e6427d96db3c433bee93e36b78116631ea8274a8
Sanchit <sanchitrrk@gmail.com>
2021-10-09 01:00:31 +0530

    migrate few routes to setu-bridge and fix server start bug to use gunicorn instead of inbuilt

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import logging
 import os
 
 from setu import app
 
-if __name__ == "__main__":
+if __name__ != "__main__":
     # When running locally, disable OAuthlib's HTTPs verification.
     # ACTION ITEM for developers:
     #     When running in production *do not* leave this option enabled.
@@ -10,4 +11,9 @@ if __name__ == "__main__":
 
     # Specify a hostname and port that are set as a valid redirect URI
     # for your API project in the Google API Console.
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+    app.logger.info("setu peacemaker service started...")
+else:
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- setu/__init__.py
+++ setu/__init__.py
@@ -112,47 +112,47 @@ def ok():
     return "ok, all good!"
 
 
-@app.route("/Consent/Notification", methods=["POST"])
-def consent_notification():
-    """
-    After we get notification from the SETU POST webhook, at this stage we
-    get the consent id which we store in the workflow item db, to main workflow state
-    Trigger next step of the AA workflow identified by the workflow id in our database.
-    """
-    data = request.get_json(force=True)
-    print("--- received data ---")
-    print(data)
-    print("---------------------")
-    consent_status_notification = data["ConsentStatusNotification"]
-    consent_status = consent_status_notification["consentStatus"]
-    print(f"solving for happy cases the consent status is {consent_status}")
+# @app.route("/Consent/Notification", methods=["POST"])
+# def consent_notification():
+#     """
+#     After we get notification from the SETU POST webhook, at this stage we
+#     get the consent id which we store in the workflow item db, to main workflow state
+#     Trigger next step of the AA workflow identified by the workflow id in our database.
+#     """
+#     data = request.get_json(force=True)
+#     print("--- received data ---")
+#     print(data)
+#     print("---------------------")
+#     consent_status_notification = data["ConsentStatusNotification"]
+#     consent_status = consent_status_notification["consentStatus"]
+#     print(f"solving for happy cases the consent status is {consent_status}")
 
-    consent_id = consent_status_notification["consentId"]  # use this to get a signed consent
-    consent_handle = consent_status_notification[
-        "consentHandle"
-    ]  # use this fot get the workflow item
-    #
-    # make database connection to get the workflow item for the received consent handle
-    projection = {"_id": False, "userRef": True, "workflowId": True}
-    workflow_item_doc = mongodb.get_collection("aaSetuWorkflows").find_one(
-        {"consentFlow.consentHandle": consent_handle}, projection=projection
-    )
-    if not workflow_item_doc:
-        print("oops this should not happen!")
-        raise ValueError("consent handle or workflow item does not exists.")
-    #
-    # save the consent response from SETU notification in store - required later for next states
-    workflow_id = workflow_item_doc["workflowId"]
-    user_ref = workflow_item_doc["userRef"]
-    print(f"update workflow with consent id for workflowId {workflow_id} and userRef {user_ref}")
+#     consent_id = consent_status_notification["consentId"]  # use this to get a signed consent
+#     consent_handle = consent_status_notification[
+#         "consentHandle"
+#     ]  # use this fot get the workflow item
+#     #
+#     # make database connection to get the workflow item for the received consent handle
+#     projection = {"_id": False, "userRef": True, "workflowId": True}
+#     workflow_item_doc = mongodb.get_collection("aaSetuWorkflows").find_one(
+#         {"consentFlow.consentHandle": consent_handle}, projection=projection
+#     )
+#     if not workflow_item_doc:
+#         print("oops this should not happen!")
+#         raise ValueError("consent handle or workflow item does not exists.")
+#     #
+#     # save the consent response from SETU notification in store - required later for next states
+#     workflow_id = workflow_item_doc["workflowId"]
+#     user_ref = workflow_item_doc["userRef"]
+#     print(f"update workflow with consent id for workflowId {workflow_id} and userRef {user_ref}")
 
-    update_fields = {"consentFlow.consentId": consent_id}
-    result = mongodb.get_collection("aaSetuWorkflows").update_one(
-        {"workflowId": workflow_id, "userRef": user_ref},
-        {"$set": update_fields},
-    )
-    print(f"updated collection aaSetuWorkflows matched count: {result.matched_count}")
-    return jsonify({"workflow_id": workflow_id})
+#     update_fields = {"consentFlow.consentId": consent_id}
+#     result = mongodb.get_collection("aaSetuWorkflows").update_one(
+#         {"workflowId": workflow_id, "userRef": user_ref},
+#         {"$set": update_fields},
+#     )
+#     print(f"updated collection aaSetuWorkflows matched count: {result.matched_count}")
+#     return jsonify({"workflow_id": workflow_id})
 
 
 @app.route("/-/2", methods=["POST"])
@@ -239,6 +239,11 @@ def request_fi_data():
     At this stage, all the actors FIP, AA FIU are aware of the user consent.
     Now we request for the financial information, for the consent - this makes sure the
     FIP starts to prepare the data at their end.
+
+
+    Note:
+        also create a async tasks with scheduled time to call for prepared/ready data
+        helpful when the POST /FI/Notification webhook doesn't work
     """
     data = request.get_json(force=True)
     print("--- received data ---")
@@ -295,41 +300,60 @@ def request_fi_data():
     return jsonify({"workflow_id": workflow_id})
 
 
-@app.route("/FI/Notification", methods=["POST"])
-def fi_notification():
+# @app.route("/FI/Notification", methods=["POST"])
+# def fi_notification():
+#     """
+#     once the data is prepared by the FIP, it notifies AA which then
+#     notifies us via this POST webhook
+
+#     Note: sometimes we notice that this hook which tells that data is prepared is not triggered by SETU
+#     perhaps it could be because of the sandbox issue
+#     """
+#     data = request.get_json(force=True)
+#     print("--- received data ---")
+#     print(data)
+#     print("---------------------")
+
+#     fi_status_notification = data["FIStatusNotification"]
+#     session_status = fi_status_notification["sessionStatus"]
+#     print(f"got FI Notification with session status {session_status}")
+
+#     session_id = fi_status_notification["sessionId"]
+#     projection = {"_id": False, "userRef": True, "workflowId": True}
+#     workflow_item_doc = mongodb.get_collection("aaSetuWorkflows").find_one(
+#         {"dataFlow.sessionId": session_id}, projection=projection
+#     )
+
+#     if not workflow_item_doc:
+#         print("oops this should not happen!")
+#         raise ValueError("consent handle or workflow item does not exists.")
+
+#     workflow_id = workflow_item_doc["workflowId"]
+#     userRef = workflow_item_doc["userRef"]
+
+#     print(f"FI notification for workflowId {workflow_id} and userRef {userRef}")
+#     #
+#     # attach our internal workflowId identifier which contains the stored states
+#     pubsub_data = {"workflowId": workflow_id}
+#     topic_path = publisher.topic_path(PROJECT_ID, AA_FI_READY_TOPIC)
+#     pubsub_data_json_string = json.dumps(pubsub_data).encode("utf-8")
+#     future = publisher.publish(topic_path, pubsub_data_json_string)
+#     print(future.result())
+#     print(f"Published message to topic {topic_path}.")
+#     return jsonify({"workflow_id": workflow_id})
+
+
+@app.route("/taskhandler", methods=["POST"])
+def taskhandler():
     """
-    once the data is prepared by the FIP, it notifies AA which then
-    notifies us via this POST webhook
+    Yet to be implemented, this will be used to trigger a pub sub event that will
+    contain the workflowId that identifies the workflow states.
+    This will get inturn trigger subscriber that will process the fi ready data.
+    Note: that this is used in case POST /FI/Notification is not triggered by SETU
     """
     data = request.get_json(force=True)
+    print("THIS SHOULD BE CALLED BY THE ASYNC TASK...")
     print("--- received data ---")
     print(data)
     print("---------------------")
-
-    fi_status_notification = data["FIStatusNotification"]
-    session_status = fi_status_notification["sessionStatus"]
-    print(f"got FI Notification with session status {session_status}")
-
-    session_id = fi_status_notification["sessionId"]
-    projection = {"_id": False, "userRef": True, "workflowId": True}
-    workflow_item_doc = mongodb.get_collection("aaSetuWorkflows").find_one(
-        {"dataFlow.sessionId": session_id}, projection=projection
-    )
-
-    if not workflow_item_doc:
-        print("oops this should not happen!")
-        raise ValueError("consent handle or workflow item does not exists.")
-
-    workflow_id = workflow_item_doc["workflowId"]
-    userRef = workflow_item_doc["userRef"]
-
-    print(f"FI notification for workflowId {workflow_id} and userRef {userRef}")
-    #
-    # attach our internal workflowId identifier which contains the stored states
-    data["workflowId"] = workflow_id
-    topic_path = publisher.topic_path(PROJECT_ID, AA_FI_READY_TOPIC)
-    data_json_string = json.dumps(data).encode("utf-8")
-    future = publisher.publish(topic_path, data_json_string)
-    print(future.result())
-    print(f"Published message to topic {topic_path}.")
-    return jsonify({"workflow_id": workflow_id})
+    return "Yet to be implemented!"
